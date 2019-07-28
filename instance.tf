da0f317be1e188da3fbd464f48151f2bc9ce6e23
khuslentuguldur <khuslen.cyber@gmail.com>
2019-07-28 17:08:59 -0500

    updated

--- instance.tf
+++ instance.tf
@@ -2,7 +2,7 @@ resource "aws_instance" "web1" {
   ami             = "ami-082b5a644766e0e6f"
   instance_type   = "t2.micro"
   key_name        = "${aws_key_pair.terraform-first.key_name}"
-  security_groups = ["${aws_security_group.sec1.id}"]
+  security_groups = ["${aws_security_group.sec1.name}"]
 
   tags = {
     Name = "Hello_Abdul"
