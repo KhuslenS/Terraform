76f4b2ab5558b054d6597d0e617788dd3ef529b7
khuslentuguldur <khuslen.cyber@gmail.com>
2019-07-28 16:42:57 -0500

    updated

--- sg.tf
+++ sg.tf
@@ -10,4 +10,10 @@ resource "aws_security_group" "sec1" {
     cidr_blocks = ["0.0.0.0/0", "192.168.1.0/32"]
 
   }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "TCP"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
