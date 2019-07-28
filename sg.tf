2cac2877733ed6843df6445a9c124177b688d3dd
khuslentuguldur <khuslen.cyber@gmail.com>
2019-07-28 16:37:44 -0500

    updated

--- sg.tf
+++ sg.tf
@@ -1,23 +1,13 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = "${aws_vpc.main.id}"
+resource "aws_security_group" "sec1" {
+  name        = "sec1"
+  description = "Allow SSH"
+  vpc_id      = "${vpc-064fe67e}"
 
   ingress {
-    # TLS (change to whatever ports you need)
-    from_port   = 443
-    to_port     = 443
+  from_port = 22
+    to_port     = 22
     protocol    = "-1"
-    # Please restrict your ingress to only necessary IPs and ports.
-    # Opening to 0.0.0.0/0 can lead to security vulnerabilities.
-    cidr_blocks = # add a CIDR block here
-  }
+    cidr_blocks = ["0.0.0.0/0" "192.168.1.0/0"]
 
-  egress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    cidr_blocks     = ["0.0.0.0/0"]
-    prefix_list_ids = ["pl-12c4e678"]
   }
 }
