f3460eac15e1fcaa73e7264d2a8252a96fe251f6
khuslentuguldur <khuslen.cyber@gmail.com>
2019-07-28 17:04:49 -0500

    updated

--- instance.tf
+++ instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web1" {
   ami            = "ami-082b5a644766e0e6f"
   instance_type  = "t2.micro"
-  key_name       = "${aws_key_pair.terroform}"
+  key_name       = "${aws_key_pair.terraform-first}"
   security_group = ["${aws_security_group.sec1.id}"]
 
   tags = {
