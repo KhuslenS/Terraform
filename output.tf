e067fca26a0bce2fab3f44a035bf25f31c4578fb
khuslentuguldur <khuslen.cyber@gmail.com>
2019-07-31 19:04:09 -0500

    updated

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "key_name" {
-  value = "${aws_key_pair.terraform_april}"
+  value = "${aws_key_pair.terraform-first.key_name}"
+}
+
+output "bucketname" {
+  value = "${aws_s3_bucket.b.bucket}"
 }
