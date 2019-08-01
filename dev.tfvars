af5d007f95367e6a690b23c8e22f2cefd612496c
khuslentuguldur <khuslen.cyber@gmail.com>
2019-07-31 20:34:41 -0500

    updated

---
+++ dev.tfvars
@@ -0,0 +1,2 @@
+ami           = "ami-082b5a644766e0e6f"
+instance_type = "t2.micro"
--- instance.tf
+++ instance.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "web1" {
-  ami             = "ami-082b5a644766e0e6f"
-  instance_type   = "t2.micro"
+  ami             = "${var.ami}"
+  instance_type   = "${var.instance_type}"
   key_name        = "${aws_key_pair.terraform-first.key_name}"
   security_groups = ["${aws_security_group.sec1.name}"]
   user_data       = "${file("userdata.sh")}"
