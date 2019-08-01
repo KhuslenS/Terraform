4690cef1b2737030a6598884c533bf2ac2350f7b
khuslentuguldur <khuslen.cyber@gmail.com>
2019-07-31 20:13:11 -0500

    updated

--- backend.tf
+++ backend.tf
@@ -1,8 +1,9 @@
 terraform {
 
   backend "s3" {
-    bucket = "terraform-april-2019-chris"
-    region = "us-east-1"
-    key    = "dev.tfstate"
+    dynamodb_table = "dev_state_lock"
+    bucket         = "terraform-april-2019-chris"
+    region         = "us-east-1"
+    key            = "dev.tfstate"
   }
 }
